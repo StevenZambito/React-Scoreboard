2e3479e5d9854618b83f6e4dd0bfb73e711fe59f
Steven Zambito <zambitosteven@gmail.com>
2020-07-13 18:24:41 -0400

    Makes Header Component with CSS Modules

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./Header.module.css";
+export const Header = () => {
+	return (
+		<div>
+			<h1 className={styles.header}>Scoreboard</h1>
+		</div>
+	);
+};
